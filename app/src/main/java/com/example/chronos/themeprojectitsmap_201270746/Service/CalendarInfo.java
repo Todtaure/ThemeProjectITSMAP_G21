3a1febf5a1450272e22cd705d3d55f7abd5d5ec9
Alex <vonanden@gmail.com>
2015-05-28 11:29:42 +0200

    fix a bug where CalendarInfo would throw an exception if calendar is empty

--- app/src/main/java/com/example/chronos/themeprojectitsmap_201270746/Service/CalendarInfo.java
+++ app/src/main/java/com/example/chronos/themeprojectitsmap_201270746/Service/CalendarInfo.java
@@ -22,41 +22,48 @@ public class CalendarInfo {
         String[] selectionArgs = new String[] {"breuer.rz@gmail.com"};
 
         mCursor = context.getContentResolver().query(CalendarContract.Events.CONTENT_URI, COLS, selection, selectionArgs, null);
-        mCursor.moveToFirst();
 
-        long eventStartTime = mCursor.getLong(1);
-        long eventEndTime = mCursor.getLong(2);
+        // Cursor is moved to first event, if calendar is empty moveToFirst return false
+        if(mCursor.moveToFirst()){
+            long eventStartTime = mCursor.getLong(1);
+            long eventEndTime = mCursor.getLong(2);
 
-        //long timeDifferenceInMillis = eventStartTime - startTime.getTimeInMillis();
-        int timeDifferenceInMin = (int)((eventStartTime - startTime.getTimeInMillis()) / (1000 * 60));
+            //long timeDifferenceInMillis = eventStartTime - startTime.getTimeInMillis();
+            int timeDifferenceInMin = (int)((eventStartTime - startTime.getTimeInMillis()) / (1000 * 60));
 
-        // if there is enough time until next event return 0
-        if (timeDifferenceInMin >= requiredTimeSlotInMin) {
-            return 0;
-        } else {
-            while (timeDifferenceInMin < requiredTimeSlotInMin && !mCursor.isLast())
-            {
-                mCursor.moveToNext();
-                eventStartTime = mCursor.getLong(1);
-                //timeDifferenceInMillis = eventStartTime - eventEndTime;
-                timeDifferenceInMin = (int)((eventStartTime - eventEndTime) / (1000 * 60));
-                // if there is enough time between previous and next event return time until free interval
-                if (timeDifferenceInMin >= requiredTimeSlotInMin) {
-                    int timeUntilFreeTimeSlot = (int)(eventEndTime - startTime.getTimeInMillis())/ (1000*60);
-                    return timeUntilFreeTimeSlot;
-                } // if there is not enough time before after the last event return number of minutes till the end of the event
-                else if (mCursor.isLast()){
+            // if there is enough time until next event return 0
+            if (timeDifferenceInMin >= requiredTimeSlotInMin) {
+                return 0;
+            } else {
+                while (timeDifferenceInMin < requiredTimeSlotInMin && !mCursor.isLast())
+                {
+                    mCursor.moveToNext();
+                    eventStartTime = mCursor.getLong(1);
+                    //timeDifferenceInMillis = eventStartTime - eventEndTime;
+                    timeDifferenceInMin = (int)((eventStartTime - eventEndTime) / (1000 * 60));
+                    // if there is enough time between previous and next event return time until free interval
+                    if (timeDifferenceInMin >= requiredTimeSlotInMin) {
+                        int timeUntilFreeTimeSlot = (int)(eventEndTime - startTime.getTimeInMillis())/ (1000*60);
+                        return timeUntilFreeTimeSlot;
+                    } // if there is not enough time before after the last event return number of minutes till the end of the event
+                    else if (mCursor.isLast()){
+                        return (int)((mCursor.getLong(2) - startTime.getTimeInMillis())/(1000 * 60));
+                    }
+                    eventEndTime = mCursor.getLong(2);
+                }
+                // if only one event in calendar return minutes until end of event
+                if (mCursor.isLast()){
                     return (int)((mCursor.getLong(2) - startTime.getTimeInMillis())/(1000 * 60));
                 }
-                eventEndTime = mCursor.getLong(2);
-            }
-            // if only one event in calendar return minutes until end of event
-            if (mCursor.isLast()){
-                return (int)((mCursor.getLong(2) - startTime.getTimeInMillis())/(1000 * 60));
-            }
-            return -1;
+                return -1;
 
+            }
         }
+        else {
+            return 0;
+        }
+
+
     }
 
 }
